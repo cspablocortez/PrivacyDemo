26b2b48fbf79f68053d876a1196a582459f19c97
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-08-04 16:42:41 -0700

    ip addr

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ async function getCity() {
       const response = await fetch('https://ipinfo.io/json?token=4a3497ed17ff2c');
       const data = await response.json();
       document.querySelector('.location').innerText = data.city ? data.city : "City not found";
+
+      document.querySelector('.ip').innerText = data.ip ? data.ip : "IP not found";
+
     } catch (error) {
       document.querySelector('.location').innerText = "UNKNOWN";
     }
